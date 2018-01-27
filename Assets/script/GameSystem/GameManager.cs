d6887abba28ffe858f183f19de24bcf371fbad5e
hsinpa <hsinpa@gmail.com>
2018-01-27 12:07:52 +0800

    Done with map generation, set old grandma waiting point, car spawn point

---
+++ Assets/script/GameSystem/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+	[HideInInspector]
+	public MapManager _mapManager;
+
+	//Where everything start
+	void Awake () {
+		_mapManager = GameObject.Find("BackGround").GetComponent<MapManager>();
+		Initialize();
+
+	}
+	
+
+	void Initialize() {
+		_mapManager.Setup(this, 5);
+	}
+
+}
---
+++ Assets/script/GameSystem/UtilityColl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Utility {
+	public class UtilityColl {
+
+		/// <summary>
+		/// Possibilities the match.
+		/// </summary>
+		/// <returns><c>true</c>, if match was possibilityed, <c>false</c> otherwise.</returns>
+		public static bool FlipCoin(float rate) {
+			float testValue = UnityEngine.Random.Range(0f ,1f);
+			return ( rate >= testValue ) ? true : false;
+		}
+
+	}
+}
